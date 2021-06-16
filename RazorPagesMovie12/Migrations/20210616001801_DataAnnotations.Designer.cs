be8c28c0a88f3cd4b6ec366131409181bfbea0f5
Scot <bscot.hagen@gmail.com>
2021-06-15 17:19:06 -0700

    Added Search, new field - Rating, Validations. Finished

--- RazorPagesMovie12/Migrations/20210616001801_DataAnnotations.Designer.cs
+++ RazorPagesMovie12/Migrations/20210616001801_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie12.Data;
 
 namespace RazorPagesMovie12.Migrations
 {
     [DbContext(typeof(RazorPagesMovie12Context))]
-    partial class RazorPagesMovie12ContextModelSnapshot : ModelSnapshot
+    [Migration("20210616001801_DataAnnotations")]
+    partial class DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
