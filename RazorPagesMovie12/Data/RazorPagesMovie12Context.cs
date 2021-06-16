09ea2e8c3a7bd867e9207cc7c5f6f2bb9d303d89
Scot <bscot.hagen@gmail.com>
2021-06-15 17:05:14 -0700

    Add project files.

---
+++ RazorPagesMovie12/Data/RazorPagesMovie12Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie12.Models;
+
+namespace RazorPagesMovie12.Data
+{
+    public class RazorPagesMovie12Context : DbContext
+    {
+        public RazorPagesMovie12Context (DbContextOptions<RazorPagesMovie12Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie12.Models.Movie> Movie { get; set; }
+    }
+}
